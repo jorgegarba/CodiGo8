e6ec4849ac9759738360c7ac8a57c03d5f801df6
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-10-17 13:00:45 -0500

    Resolucion de ejercicio de comida

--- BackEnd/Semana3/Dia4/02-condicionales.py
+++ BackEnd/Semana3/Dia4/02-condicionales.py
@@ -38,19 +38,30 @@ cadena = "Buenos dias"
 #     print(numero)
 
 # for doble
-for numero1 in range(4):
-    for numero2 in range(3):
-        print(numero1,numero2)
-
+# for numero1 in range(4):
+#     for numero2 in range(3):
+       # print(numero1,numero2)
+#
 # while -> un bucle infinito de acciones mientras sea cierta la condicion
 valor = 1
 fin = 10
-while(valor<fin):
-    print(valor)
-    valor += 1 # valor ++
+# while(valor<fin):
+#     print(valor)
+#    valor += 1 # valor ++
 # pass -> no hace nada, solo indica que pase a la siguiente iteracion
 
 # Ejercicio: que de una lista de numeros
 numeros = [1,2,3,4,5,6,7,8,9]
 #guarde todos los pares en la lista numeros_pares y los impares en la lista 
 # numeros_impares y que muestre las listas y la lista de numeros debe quedar vacia
+numeros_pares=[]
+numeros_impares=[]
+for numero in numeros:
+    if(numero%2==0):
+        numeros_pares.append(numero)
+    else:
+        numeros_impares.append(numero)
+print(numeros_pares)
+print(numeros_impares)
+numeros.clear()
+print(numeros)
