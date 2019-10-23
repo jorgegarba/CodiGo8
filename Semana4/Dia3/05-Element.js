0a12831d8cf190fb0b67af00cb87c2aca10e8d52
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-10-23 17:30:31 -0500

    Avance tarea

--- Semana4/Dia3/05-Element.js
+++ Semana4/Dia3/05-Element.js
@@ -25,3 +25,37 @@ for(let i=0;i<5;i++){
 // Al final de la tabla mostrar el total de la compra
 // y luego un parrafo que indique si es mayo de 50 soles puede pagar en efectivo o con tarjeta
 // y si es menor de 50 soles, solo puede pagar con efectivo.
+
+
+// Caputar el elemento donde vamos a insertar la tabla
+var seccion = document.getElementById("miSeccion");
+// Pidiendo n elementos que va a tener mi boleta
+var n = +prompt("Ingrese la cantidad de elementos");
+// Creamos la tabla
+var tabla = document.createElement("table");
+// Creamos la cabecera
+var cabecera = document.createElement("tr")
+// Crear las columnas para la cabecera
+var thNro = document.createElement("th");
+thNro.innerHTML="Nro."
+var thCant = document.createElement("th");
+thCant.innerHTML="Cantidad"
+var thDesc = document.createElement("th");
+thDesc.innerHTML="Descripcion"
+var thPreUni = document.createElement("th");
+thPreUni.innerHTML="P. Unitario"
+var thTotal = document.createElement("th");
+thTotal.innerHTML="Total"
+
+// Inserta las columas a la cabecera
+cabecera.appendChild(thNro);
+cabecera.appendChild(thCant);
+cabecera.appendChild(thDesc);
+cabecera.appendChild(thPreUni);
+cabecera.appendChild(thTotal);
+
+// Insertar la cabecera a la tabla
+tabla.appendChild(cabecera);
+// Insertamos la tabla a la seccion
+seccion.appendChild(tabla);
+tabla.border=1;
