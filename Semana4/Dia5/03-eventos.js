cf65b76d97c04092fe8a1b8780bcbbeb4f9d365e
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-10-25 12:56:09 -0500

    Subiendo Tarea

--- Semana4/Dia5/03-eventos.js
+++ Semana4/Dia5/03-eventos.js
@@ -80,6 +80,7 @@ ul.appendChild(li2);
 divMenu.appendChild(ul);
 divMenu.style.position = "absolute";
 divMenu.style.visibility = "hidden";
+divMenu.classList.add("mi-menu");
 
 
 let miBody = document.getElementById("miBody");
@@ -109,3 +110,34 @@ miBody.oncontextmenu = (evento) => {
 miBody.onclick = () => {
   divMenu.style.visibility = 'hidden';
 }
+
+
+
+
+// TAREITA
+let paises = [
+  { idPais: 1, nombrePais: 'Perú', },
+  { idPais: 2, nombrePais: 'Bolivia', },
+  { idPais: 3, nombrePais: 'Brasil', }
+];
+let ciudades = [
+  { idCiudad: 1, idPais: 1, nombreCiudad: "Lima" },
+  { idCiudad: 2, idPais: 1, nombreCiudad: "Puno" },
+  { idCiudad: 3, idPais: 2, nombreCiudad: "La Paz" },
+  { idCiudad: 4, idPais: 2, nombreCiudad: "Cochabamba" },
+  { idCiudad: 5, idPais: 3, nombreCiudad: "Sao Paulo" },
+  { idCiudad: 6, idPais: 3, nombreCiudad: "Brasilia" },
+]
+
+// crear un select donde figuren todos los paises
+
+// crear un select donde figuren las ciudades del
+// pais seleccionado
+// Si no hay pais seleccionado el select de las ciudades
+// debera estar bloqueado
+// select.setAttribute("disabled",true);
+
+// Crear un boton debajo de ambos selects
+// el boton solo debe responder al click e imprmiir
+// el nombre del pais y la ciudad seleccionada
+// cuando ambos selects tengan valores reales
