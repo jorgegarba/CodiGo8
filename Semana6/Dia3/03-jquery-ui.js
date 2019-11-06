a474451ff90e2520c6f5426deafe8f07b145c377
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-11-06 15:44:41 -0500

    JQuery UI

--- Semana6/Dia3/03-jquery-ui.js
+++ Semana6/Dia3/03-jquery-ui.js
@@ -2,15 +2,17 @@ $(".cuadrado").draggable();
 $("#contenido").draggable();
 
 
+$("#contenedor").resizable();
+
 $("#contenedor").droppable(
   {
     // drop => evento que se dispara cuando un elemento
     // es soltado en el área del contenedor
     drop: function (event, ui) {
-
       // ui => el elemento que llega al contenedor
-      console.log(ui.draggable[0]);
+      let hijo = ui.draggable[0];
       console.log("UF! soltaron un elementillo");
+      console.log(hijo.getAttribute("colorcito"));
     }
   }
 );
