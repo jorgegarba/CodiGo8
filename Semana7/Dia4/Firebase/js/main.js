e3e551cd913ac2fe9658044b4af10f1fa8b50262
Jorge Garnica <jorgegarba@gmail.com>
2019-11-14 12:24:18 -0500

    Firebase Project

---
+++ Semana7/Dia4/Firebase/js/main.js
@@ -0,0 +1,12 @@
+import { firebaseConfig } from './env/config.js';
+
+// INICIALIZAR FIREBASE
+firebase.initializeApp(firebaseConfig);
+// Instanciar la base de datos
+let database = firebase.database();
+
+let postCancha = (objCancha) => {
+
+}
+
+
