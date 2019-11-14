4fd39e1aa1758e0468e264791978504e1360a795
Jorge Garnica <jorgegarba@gmail.com>
2019-11-14 18:39:36 -0500

    starter con firebase

--- Semana7/Dia4/Firebase/js/main.js
+++ Semana7/Dia4/Firebase/js/main.js
@@ -5,8 +5,43 @@ firebase.initializeApp(firebaseConfig);
 // Instanciar la base de datos
 let database = firebase.database();
 
+
 let postCancha = (objCancha) => {
 
 }
+
+
+// SCRIPT PARA CANCHAS.HTML
+if (document.location.href.indexOf("canchas.html") != -1) {
+
+  // ESTOY EN LA PAGINA CANCHAS. HTML
+  $("#btnAbrirModal").click(function () {
+    // mostrar modal cuando se hace click en el botón circular
+    $("#modalCrearCancha").modal("show");
+  })
+
+  $("#frmCrearCancha").submit(function (event) {
+    event.preventDefault();
+    console.log("submit!!!");
+    
+    // Armar el objeto JSON
+    let objCancha = {
+      nombre: $("#inputNombre").val(),
+      direccion: $("#inputDireccion").val(),
+      lat: $("#inputLatitud").val(),
+      lng: $("#inputLongitud").val(),
+      nrocanchas: $("#inputNroCanchas").val(),
+      telefono: $("#inputTelefono").val()
+    }
+    console.log(objCancha);
+    
+
+  })
+
+}
+
+
+
+
 
 
