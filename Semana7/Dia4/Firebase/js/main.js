3658fbb9b44b83ef7369edf13ea394ce7aee9725
Jorge Garnica <jorgegarba@gmail.com>
2019-11-15 11:58:12 -0500

    CRUD FIREBASE 50%

--- Semana7/Dia4/Firebase/js/main.js
+++ Semana7/Dia4/Firebase/js/main.js
@@ -5,9 +5,29 @@ firebase.initializeApp(firebaseConfig);
 // Instanciar la base de datos
 let database = firebase.database();
 
-
 let postCancha = (objCancha) => {
 
+  // Obtener una llave única para el nodo canchas
+  let key = database.ref("canchas").push().key;
+  // inyectar el objeto en el nodo canchas/key
+  database.ref("canchas").child(key)
+    .set(objCancha)
+    .then(() => {
+      Swal.fire({
+        title: 'Creado!',
+        text: 'Canchita creada correctamente',
+        icon: 'success'
+      });
+      $("#modalCrearCancha").modal("hide");
+      document.getElementById("frmCrearCancha").reset();
+    })
+    .catch(() => {
+      Swal.fire({
+        title: 'Ups!',
+        text: 'Se ha producido un error en el servidorsh',
+        icon: 'error'
+      })
+    })
 }
 
 
@@ -23,7 +43,7 @@ if (document.location.href.indexOf("canchas.html") != -1) {
   $("#frmCrearCancha").submit(function (event) {
     event.preventDefault();
     console.log("submit!!!");
-    
+
     // Armar el objeto JSON
     let objCancha = {
       nombre: $("#inputNombre").val(),
@@ -33,10 +53,60 @@ if (document.location.href.indexOf("canchas.html") != -1) {
       nrocanchas: $("#inputNroCanchas").val(),
       telefono: $("#inputTelefono").val()
     }
-    console.log(objCancha);
-    
+
+    postCancha(objCancha);
 
   })
+
+  let dibujarTabla = (arregloCanchas) => {
+    $("#tablaCanchas").DataTable({
+      data: arregloCanchas,
+      // destroy, sirve para reinicializar el datatable cada vez
+      // que se llame a la función nuevamente
+      destroy: true,
+      columns: [
+        { title: "Id", data: 'id' },
+        { title: "Nombre", data: 'nombre' },
+        { title: "Dirección", data: 'direccion' },
+        { title: "Nro lozas", data: 'nrocanchas' },
+        { title: "Lat", data: 'lat' },
+        { title: "Long", data: 'lng' },
+        { title: "Teléfono", data: 'telefono' }
+      ]
+    });
+  }
+
+  let traerDatos = () => {
+    // =======
+    // ======= Con la función ONCE => Que solo trae la data una vez
+    // =======
+    // database.ref("canchas").once('value').then((dataSnapshot) => {
+    //   let arregloCanchas = [];
+    //   dataSnapshot.forEach((cancha) => {
+    //     arregloCanchas.push({
+    //       ...cancha.val(),
+    //       id: cancha.key
+    //     })
+    //   });
+    //   dibujarTabla(arregloCanchas);
+    // })
+    // =======
+    // Con la función ON => Que escucha cualquier evento de cambio en 
+    // la BD
+    // =======
+    database.ref("canchas").on('value', (dataSnapshot) => {
+      let arregloCanchas = [];
+      dataSnapshot.forEach((cancha) => {
+        arregloCanchas.push({
+          ...cancha.val(),
+          id: cancha.key
+        })
+      });
+      dibujarTabla(arregloCanchas);
+    })
+  }
+
+  traerDatos();
 
 }
 
