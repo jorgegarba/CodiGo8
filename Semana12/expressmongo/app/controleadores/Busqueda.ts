34dc5712b01bf4c552f927fbf974cc9ef2baa43f
Jorge Garnica <jorgegarba@gmail.com>
2019-12-19 12:02:43 -0500

    Paginacion terminada

---
+++ Semana12/expressmongo/app/controleadores/Busqueda.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+
+export let buscar = (req: Request, res: Response) => {
+
+  // get /buscar/:coleccion/:termino
+
+  let { termino, tabla } = req.params;
+  let regex = /ab/;
+  
+
+  
+
+
+}
