6fd2a357b4f85f60257c4bc829de20f85f7c6259
Jorge Garnica <jorgegarba@gmail.com>
2020-01-13 11:22:50 -0500

    Subiendo video de Loggin

--- Semana14/sgp-front/src/modulos/Loggin/Loggin.js
+++ Semana14/sgp-front/src/modulos/Loggin/Loggin.js
@@ -20,7 +20,6 @@ export class Loggin extends Component {
             return <Login />
           }} />
         </Switch>
-        <Footer />
       </Fragment>
     );
   }
--- Semana14/sgp-front/src/modulos/Loggin/paginas/Login.js
+++ Semana14/sgp-front/src/modulos/Loggin/paginas/Login.js
@@ -3,9 +3,29 @@ import React, { Component } from 'react';
 export class Login extends Component {
   render() {
     return (
-      <div>
-        Login trabajando
-      </div>
+      <main className="container mt-5">
+        <div className="row">
+          <div className="col-12">
+            <form>
+              <div className="form-group">
+                <label htmlFor="">Email:</label>
+                <input type="text" className="form-control"
+                  placeholder="Ingrese su correo" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="">Password:</label>
+                <input type="password" className="form-control"
+                  placeholder="Ingrese su contraseña" />
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary btn-block">
+                  Iniciar Sesión
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
     );
   }
 }
