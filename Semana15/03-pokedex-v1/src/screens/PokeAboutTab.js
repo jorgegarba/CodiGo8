951fa811a46538329494ac97b4aa2f9863b25b5f
Jorge Garnica <jorgegarba@gmail.com>
2020-01-23 11:12:58 -0500

    Agregando vista de peso y talla

--- Semana15/03-pokedex-v1/src/screens/PokeAboutTab.js
+++ Semana15/03-pokedex-v1/src/screens/PokeAboutTab.js
@@ -8,14 +8,58 @@ const misEstilos = StyleSheet.create({
     paddingTop: 10
   },
   descripcionView: {
-    flex: 1
+    // flex: 1
+    marginBottom: 7
   },
+
   pesoTallaView: {
-    flex: 1
+    // flex: 1,
+    borderRadius: 25,
+    backgroundColor: 'white',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    padding: 15,
+    margin: 20
   },
+
   generoView: {
     flex: 1
+  },
+  talla: {
+    flex: 1
+  },
+  peso: {
+    flex: 1
+  },
+  negrita: {
+    fontWeight: 'bold',
+    color: '#666',
+    fontSize: 18
+  },
+  titulos: {
+    flexDirection: 'row'
+  },
+  valores: {
+    flexDirection: 'row'
+  },
+  tallaView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  pesoView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
+
+
 })
 export default class PokeAboutTab extends Component {
 
@@ -23,7 +67,9 @@ export default class PokeAboutTab extends Component {
     super(props);
     this.state = {
       cargando: true,
-      descripcion: ''
+      descripcion: '',
+      peso: 0,
+      talla: 0
     }
   }
 
@@ -36,11 +82,17 @@ export default class PokeAboutTab extends Component {
     let global = Global.getInstance();
     let endpoint = `https://pokeapi.co/api/v2/pokemon-species/${global.pokemonId}`;
     let rpta = await axios.get(endpoint);
-    
+
+    let endpoint2 = `https://pokeapi.co/api/v2/pokemon/${global.pokemonId}`;
+    let rpta2 = await axios.get(endpoint2);
+    let { height, weight } = rpta2.data;
+
     let descripcion = this.obtenerCastellano(rpta.data.flavor_text_entries)
     this.setState({
       descripcion: descripcion,
       cargando: false,
+      peso: weight,
+      talla: height
     })
   }
 
@@ -66,7 +118,22 @@ export default class PokeAboutTab extends Component {
         </View>
         {/* Peso y Talla */}
         <View style={misEstilos.pesoTallaView}>
-          <Text>asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</Text>
+          <View style={misEstilos.titulos}>
+            <View style={misEstilos.talla}>
+              <Text style={misEstilos.negrita}>Talla:</Text>
+            </View>
+            <View style={misEstilos.peso}>
+              <Text style={misEstilos.negrita}>Peso:</Text>
+            </View>
+          </View>
+          <View style={misEstilos.valores}>
+            <View style={misEstilos.tallaView}>
+              <Text >{this.state.talla * 10} cm .</Text>
+            </View>
+            <View style={misEstilos.pesoView}>
+              <Text >{(this.state.peso / 10).toFixed(2)} Kg.</Text>
+            </View>
+          </View>
         </View>
         {/* Genero */}
         <View style={misEstilos.generoView}>
